dccdcc154ff4a93a5233e7fe9f65392d68fbeb60
Maksim Krasutski <krasutskimaksim@gmail.com>
2026-08-18 17:21:59 +0200

    Fix odd subscene lifecycle

--- FireAlt.Core.Editor/Internal/Utils/InternalEditorRenderData.cs
+++ FireAlt.Core.Editor/Internal/Utils/InternalEditorRenderData.cs
@@ -4,6 +4,11 @@ namespace FireAlt.Core.Editor
 {
     public static class InternalEditorRenderData
     {
+        public static bool HasSceneCullingMask(EntityManager em, Entity entity)
+        {
+            return em.HasComponent<EditorRenderData>(entity);
+        }
+
         public static ulong GetSceneCullingMask(EntityManager em, Entity entity)
         {
             return em.HasComponent<EditorRenderData>(entity)
@@ -25,6 +30,11 @@ namespace FireAlt.Core.Editor
                 comp.SceneCullingMask = sceneCullingMask;
                 em.SetSharedComponent(entity, comp);
             }
+        }
+
+        public static void RemoveSceneCullingMask(EntityManager em, Entity entity)
+        {
+            if (em.HasComponent<EditorRenderData>(entity)) em.RemoveComponent<EditorRenderData>(entity);
         }
     }
 }
