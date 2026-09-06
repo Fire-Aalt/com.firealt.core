d96c69059d838645a68f15a3f134e4b097d5a6eb
Maksim Krasutski <krasutskimaksim@gmail.com>
2026-09-06 16:31:38 +0200

    Fix depends on for materials

--- FireAlt.Core/Rendering/RuntimeMaterialSystem.cs
+++ FireAlt.Core/Rendering/RuntimeMaterialSystem.cs
@@ -10,17 +10,19 @@ namespace FireAlt.Core.Rendering
     [RequireMatchingQueriesForUpdate]
     [UpdateInGroup(typeof(RuntimeBakingSystemGroup))]
     [WorldSystemFilter(WorldSystemFilterFlags.Default | WorldSystemFilterFlags.Editor)]
-    public partial class RuntimeMaterialSystem : SystemBase
+    public partial class RuntimeMaterialSystem : SystemBase 
     {
         private readonly struct BatchMaterial
         {
             public readonly Material Material;
             public readonly BatchMaterialID MaterialID;
-
-            public BatchMaterial(Material material, BatchMaterialID materialID)
+            public readonly int SrcMaterialVersion;
+            
+            public BatchMaterial(Material material, BatchMaterialID materialID, int srcMaterialDirtyCount)
             {
                 Material = material;
                 MaterialID = materialID;
+                SrcMaterialVersion = srcMaterialDirtyCount;
             }
         }
         
@@ -33,8 +35,8 @@ namespace FireAlt.Core.Rendering
             foreach (var (materialMeshInfoRW, lookupRO, enabled) in SystemAPI.Query<RefRW<MaterialMeshInfo>, RefRO<RuntimeMaterialLookup>, EnabledRefRW<RuntimeMaterialLookup>>()
                          .WithOptions(EntityQueryOptions.IncludePrefab))
             {
-                var batchMaterial = GetBatchMaterial(lookupRO.ValueRO.Value, entitiesGraphicsSystem, true);
-
+                var batchMaterial = GetBatchMaterial(lookupRO.ValueRO.Value, entitiesGraphicsSystem);
+                
                 materialMeshInfoRW.ValueRW.MaterialID = batchMaterial.MaterialID;
                 enabled.ValueRW = false;
             }
@@ -42,51 +44,58 @@ namespace FireAlt.Core.Rendering
             foreach (var (materialRW, lookupRO, enabled) in SystemAPI.Query<RefRW<RuntimeMaterial>, RefRO<RuntimeMaterialLookup>, EnabledRefRW<RuntimeMaterialLookup>>()
                          .WithOptions(EntityQueryOptions.IncludePrefab))
             {
-                var batchMaterial = GetBatchMaterial(lookupRO.ValueRO.Value, entitiesGraphicsSystem, false);
+                var batchMaterial = GetBatchMaterial(lookupRO.ValueRO.Value, entitiesGraphicsSystem);
 
                 materialRW.ValueRW.Value = batchMaterial.Material;
                 enabled.ValueRW = false;
             }
         }
 
-        private BatchMaterial GetBatchMaterial(MaterialLookup lookup, EntitiesGraphicsSystem entitiesGraphicsSystem, bool registerIfMissing)
+        // Ideally this method should be called only once for every RuntimeMaterialLookup rebake or reenable,
+        // but due to a bug in live subscene baking, this method has to be called each frame on each entity in the Edit mode
+        private BatchMaterial GetBatchMaterial(MaterialLookup lookup, EntitiesGraphicsSystem entitiesGraphicsSystem)
         {
+            var srcMaterialVersion = UnityEditor.EditorUtility.GetDirtyCount(lookup.SrcMaterial.Value);
+            
             if (!_materials.TryGetValue(lookup, out var batchMaterial))
             {
-                batchMaterial = CreateAndRegister(lookup, entitiesGraphicsSystem, registerIfMissing);
+                batchMaterial = CreateAndRegister(lookup, srcMaterialVersion, entitiesGraphicsSystem);
                 _materials.Add(lookup, batchMaterial);
             }
 #if UNITY_EDITOR
             else if (batchMaterial.Material == null)
             {
-                batchMaterial = CreateAndRegister(lookup, entitiesGraphicsSystem, registerIfMissing);
+                batchMaterial = CreateAndRegister(lookup, srcMaterialVersion, entitiesGraphicsSystem);
                 _materials[lookup] = batchMaterial;
             }
 #endif
+
+            if (srcMaterialVersion != batchMaterial.SrcMaterialVersion)
+            {
+                entitiesGraphicsSystem.UnregisterMaterial(batchMaterial.MaterialID);
+                CoreUtils.Destroy(batchMaterial.Material);
+                
+                batchMaterial = CreateAndRegister(lookup, srcMaterialVersion, entitiesGraphicsSystem);
+                _materials[lookup] = batchMaterial;
+            }
+
             return batchMaterial;
         }
 
-        private static BatchMaterial CreateAndRegister(MaterialLookup lookup, EntitiesGraphicsSystem entitiesGraphicsSystem, bool registerIfMissing)
+        private static BatchMaterial CreateAndRegister(MaterialLookup lookup, int srcMaterialVersion,
+            EntitiesGraphicsSystem entitiesGraphicsSystem)
         {
             var mat = SpriteMaterialUtility.CloneFromLookup(lookup);
+            var matId = entitiesGraphicsSystem.RegisterMaterial(mat);
             
-            BatchMaterialID matId = default;
-            if (registerIfMissing)
-                matId = entitiesGraphicsSystem.RegisterMaterial(mat);
-            
-            return new BatchMaterial(mat, matId);
+            return new BatchMaterial(mat, matId, srcMaterialVersion);
         }
 
         protected override void OnDestroy()
         {
             foreach (var kvp in _materials)
             {
-#if UNITY_EDITOR
-                if (!Application.isPlaying)
-                    Object.DestroyImmediate(kvp.Value.Material);
-                else
-#endif
-                    Object.Destroy(kvp.Value.Material);
+                CoreUtils.Destroy(kvp.Value.Material);
             }
         }
     }
